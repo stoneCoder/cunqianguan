baa0d8ac679fc099406a76f65ba716e92b88cacf
stone.zhang <sisanyiba@sisanyibadeiMac.local>
2015-01-20 15:17:31 +0800

    add GridMenu

---
+++ cunqianguan/Class/ExpandClass/HomePage/View/GridMenu.h
@@ -0,0 +1,13 @@
+//
+//  GridMenu.h
+//  cunqianguan
+//
+//  Created by 四三一八 on 15/1/20.
+//  Copyright (c) 2015年 4318. All rights reserved.
+//
+
+#import "BaseCollectionView.h"
+
+@interface GridMenu : BaseCollectionView
+
+@end
